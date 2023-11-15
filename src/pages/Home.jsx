944fe09ed17958171c5dc299d57c9b73d6ecfc01
AndreaLindaas <112169120+AndreaLindaas@users.noreply.github.com>
2023-11-15 13:59:59 +0100

    added text part

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,17 @@
 import { Button } from "@mui/material";
+import "./Home.scss";
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <div className="sloagen-container">
+        <p>Bidding with a bit of fun</p>
+        <h1>BidBitz</h1>
+        <p>
+          Welcome to BidBits- the simple and fun way to buy and sell items
+          online. You can find items in allmost all catagories, from small beads
+          to your new house. Not into buying?Sell almost anything you want!
+        </p>
+      </div>
+    </>
+  );
 }

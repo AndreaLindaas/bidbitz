9bd847b790f0d044bb33fc950769e0cae5946314
AndreaLindaas <112169120+AndreaLindaas@users.noreply.github.com>
2023-11-19 15:33:08 +0100

    made componentplacebid

--- src/components/PlaceBid/PlaceBid.jsx
+++ src/components/PlaceBid/PlaceBid.jsx
@@ -1,18 +1,35 @@
 import { TextField } from "@mui/material";
 import { Button } from "@mui/material";
+import { Link } from "react-router-dom";
 export default function PlaceBid() {
+  const userEmail = localStorage.getItem("user_email");
+
+  if (userEmail) {
+    return (
+      <>
+        <TextField
+          id="standard-basic"
+          label="Amount"
+          variant="standard"
+          type="number"
+        />
+        <div>
+          <Button className="primary bid-btn" variant="contained">
+            Place bid
+          </Button>
+        </div>
+      </>
+    );
+  }
   return (
     <>
-      <TextField
-        id="standard-basic"
-        label="Amount"
-        variant="standard"
-        type="number"
-      />
+      <div>Do you want to bid on this item? Login to place a bid!</div>
       <div>
-        <Button className="primary bid-btn" variant="contained">
-          Place bid
-        </Button>
+        <Link to="/login">
+          <Button className="primary bid-btn" variant="contained">
+            Login
+          </Button>
+        </Link>
       </div>
     </>
   );

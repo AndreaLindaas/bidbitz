61ef0da348bb49e67335a1b426df92725b70ce88
AndreaLindaas <112169120+AndreaLindaas@users.noreply.github.com>
2023-11-20 13:48:47 +0100

    added bidding functionality

--- src/components/PlaceBid/PlaceBid.jsx
+++ src/components/PlaceBid/PlaceBid.jsx
@@ -1,33 +1,70 @@
 import { TextField } from "@mui/material";
 import { Button } from "@mui/material";
 import { Link } from "react-router-dom";
-
+import { API_URL } from "../../lib/constants";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useParams } from "react-router-dom";
 export default function PlaceBid(props) {
   const userEmail = localStorage.getItem("user_email");
+  const accessToken = localStorage.getItem("access_token");
+  const [errorMessage, setErrorMessage] = useState("");
+  const navigate = useNavigate();
+  const params = useParams();
 
-  console.log(props);
-  const placeBidClicked = () => {
-    console.log("halllooooo");
+  const placeBidClicked = async (event) => {
+    event.preventDefault();
+    const amount = event.target.elements;
+    const bidAmount = amount.amount.value;
+    console.log(props);
+
+    try {
+      const res = await fetch(`${API_URL}/listings/${params.listingId}/bids`, {
+        method: "POST",
+        body: JSON.stringify({ amount: parseFloat(bidAmount) }),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+
+      const data = await res.json();
+
+      if (data.statusCode > 300) {
+        setErrorMessage(data.errors[0].message);
+      } else {
+        navigate("#");
+      }
+    } catch (error) {
+      console.warn("An error occurred", error);
+    }
+    if (bidAmount > props) {
+      bidAmount;
+    }
+    return <div>Amount to small</div>;
   };
 
   if (userEmail) {
     return (
       <>
-        <TextField
-          id="standard-basic"
-          label="Amount"
-          variant="standard"
-          type="number"
-        />
-        <div>
-          <Button
-            onClick={placeBidClicked}
-            className="primary bid-btn"
-            variant="contained"
-          >
-            Place bid
-          </Button>
-        </div>
+        <form onSubmit={placeBidClicked}>
+          <TextField
+            id="standard-basic"
+            label="Amount"
+            name="amount"
+            variant="standard"
+            type="number"
+          />
+          <div>
+            <Button
+              className="primary bid-btn"
+              variant="contained"
+              type="submit"
+            >
+              Place bid
+            </Button>
+          </div>
+        </form>
       </>
     );
   }
@@ -41,6 +78,7 @@ export default function PlaceBid(props) {
           </Button>
         </Link>
       </div>
+      {errorMessage}
     </>
   );
 }

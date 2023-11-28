116ba6a604c9a60a9cbf8092718f11227d873243
AndreaLindaas <112169120+AndreaLindaas@users.noreply.github.com>
2023-11-28 21:19:27 +0100

    added sortBy and cleaned code

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,47 +1,17 @@
-import { TextField, MenuItem } from "@mui/material";
 import PropTypes from "prop-types";
 import "./Filter.scss";
 import SortOrder from "./SortOrder";
+import SortBy from "./SortBy";
 Filter.propTypes = {
   sortBy: PropTypes.func,
   sortOrder: PropTypes.func,
 };
 export default function Filter(props) {
   const { sortBy, sortOrder } = props;
-  const showSortByFilter = () => {
-    const sortItems = [
-      {
-        value: "created",
-        label: "Created",
-      },
-      {
-        value: "endsAt",
-        label: "Ending",
-      },
-    ];
-    return sortItems.map((option) => (
-      <MenuItem key={option.value} value={option.value}>
-        {option.label}
-      </MenuItem>
-    ));
-  };
-  const sortByChanged = (event) => {
-    const filter = event.target.value;
-    sortBy(filter);
-  };
 
   return (
     <>
-      <TextField
-        id="outlined-select-currency"
-        select
-        defaultValue={"endsAt" || ""}
-        label="Sort by"
-        onChange={sortByChanged}
-        className="filter"
-      >
-        {showSortByFilter()}
-      </TextField>
+      <SortBy sortBy={sortBy} />
       <SortOrder sortOrder={sortOrder} />
     </>
   );
